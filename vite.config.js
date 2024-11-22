de78d5cb4f901d48a44e4e9296ccaa4e71f728ec
karen371 <karengodoy325@gmail.com>
2024-11-22 19:47:55 -0300

    cambios para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/Prueba-Desarrollo-de-interfaces/', // Nombre del repositorio
 })
