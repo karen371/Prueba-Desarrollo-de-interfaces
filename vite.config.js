1798ce060ed56fe1a7b08eb5c6def46773e45c38
karen371 <karengodoy325@gmail.com>
2024-11-22 21:00:57 -0300

    cambio en el link

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://karen371/Prueba-Desarrollo-de-interfaces/', // Nombre del repositorio
+  base: 'https://karen371.github.io/Prueba-Desarrollo-de-interfaces/', // Nombre del repositorio
 })
